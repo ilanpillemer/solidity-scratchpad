1a86a15bf00550a32665346d23928b59e16b0b3a
Ilan Pillemer <ilan.pillemer@gmail.com>
2017-09-01 21:22:53 +0100

    Owned

---
+++ Owned.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.0;
+contract Owned {
+     address public owner;
+
+    modifier only_owner() {
+        if (msg.sender == owner) {
+            _;
+        }
+    }
+   event OwnerChanged(address oldOwner,address newOwner);
+
+    function Owned() {
+        owner = msg.sender;
+    }
+
+    function changeOwner(address _newOwner) external only_owner {
+        address oldOwner = owner;
+        owner = _newOwner;
+        OwnerChanged(oldOwner,_newOwner);
+    }
+}
